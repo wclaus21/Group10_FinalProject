e6105caa36e174a09591f16a34740cb1bc702eea
wclaus21 <clausws@mail.uc.edu>
2024-04-21 20:38:33 -0400

    easy progress

--- Group10_FinalProject/Easy.aspx
+++ Group10_FinalProject/Easy.aspx
@@ -9,6 +9,16 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:Table ID="Table1" runat="server"></asp:Table>
+            <asp: Table Row>
+                <asp: Table Column>
+
+            You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+            <asp:Button ID="Button1" runat="server" Text="Button" />
         </div>
     </form>
 </body>
--- Group10_FinalProject/Easy.aspx.cs
+++ Group10_FinalProject/Easy.aspx.cs
@@ -10,8 +10,72 @@ namespace Group10_FinalProject
     public partial class Easy : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
+        { } }
+            
 
+namespace group5_FinalProject
+    {
+        public class LeetCode224Solution
+        {
+            public int Calculate(string s)
+            {
+                Stack<int> stack = new Stack<int>();
+                int currentNumber = 0;
+                int result = 0; // For the current result
+                int sign = 1;  // 1 means positive, -1 means negative  
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    char currentChar = s[i];
+
+                    if (Char.IsDigit(currentChar))
+                    {
+                        // Parse the whole number
+                        currentNumber = (currentChar - '0');
+
+                        while (i + 1 < s.Length && Char.IsDigit(s[i + 1]))
+                        {
+                            currentNumber = currentNumber * 10 + (s[i + 1] - '0');
+                            i++;
+                        }
+
+                        currentNumber *= sign;
+                        result += currentNumber;
+                        currentNumber = 0; // reset number to 0 before next potential number
+                        sign = 1; // reset sign to positive
+                    }
+                    else if (currentChar == '+')
+                    {
+                        sign = 1;
+                    }
+                    else if (currentChar == '-')
+                    {
+                        sign = -1;
+                    }
+                    else if (currentChar == '(')
+                    {
+                        // Push the result and the sign onto the stack, for later
+                        // We push the result first, then the sign
+                        stack.Push(result);
+                        stack.Push(sign);
+
+                        // Reset the sign and result for the new subexpression
+                        sign = 1;
+                        result = 0;
+                    }
+                    else if (currentChar == ')')
+                    {
+                        // Pop the sign and the temporary result from the stack
+                        // Multiply the current result by the sign on the top of the stack
+                        // Then add to the next top, which is the temporary result before this pair of parentheses
+                        result *= stack.Pop(); // stack.pop() is the sign before the parenthesis
+                        result += stack.Pop(); // stack.pop() is the result calculated before the parenthesis
+                    }
+                }
+
+                return result;
+            }
         }
     }
 }
+    
